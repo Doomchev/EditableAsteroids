52faabc5297ce4319f38be2309d25680080bc4e8
Matvey Doomchev <32887734+doomchev@users.noreply.github.com>
2022-11-24 20:17:15 +1100

    0.6.7 highlighting for actions

--- src/main/kotlin/mod/drawing/drawBlocks.kt
+++ src/main/kotlin/mod/drawing/drawBlocks.kt
@@ -1,16 +1,24 @@
 package mod.drawing
 
 import blocks
+import currentCanvas
 import mod.dragging.Drawing
+import mousefy
+import mousesy
 import java.awt.Color
 import java.awt.Graphics2D
 
 object drawBlocks: Drawing {
   override fun draw(g: Graphics2D) {
     g.color = Color.white
-    var y = 16
+    var y = 0
     for(block in blocks) {
-      g.drawString(block.message, 0, y)
+      if(mousesy >= y && mousesy < y + 16) {
+        g.color = Color.GRAY
+        g.fillRect(0, y, currentCanvas.viewport.width, 16)
+        g.color = Color.white
+      }
+      g.drawString(block.message, 0, y + 12)
       y += 16
     }
     g.color = Color.black
